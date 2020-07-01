97d19ddd7fedd630b095e0a6822a9d629b5c7bc2
matsuoka <grapevine0617@gmail.com>
2020-07-01 15:04:52 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :liked_user, through: :favorites, source: :user
   validates :content, presence: true, length: { maximum: 255 }
 end
